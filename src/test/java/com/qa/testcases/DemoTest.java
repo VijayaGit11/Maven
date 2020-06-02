73e4fe8aa34a17e0914336ad5103f1b334149b18
sachpatil20 <sachpatil20@gmail.com>
2020-06-02 20:51:04 +0530

    uploading maven project

---
+++ src/test/java/com/qa/testcases/DemoTest.java
@@ -0,0 +1,35 @@
+package com.qa.testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	
+	public static int a =10;
+	public static int b=20;
+	
+	@Test
+	public void sum() {
+		System.out.println("Sum");
+		Assert.assertEquals(30, a+b);
+	}
+	
+	@Test
+	public void sub() {
+		System.out.println("Substraction");
+		Assert.assertEquals(10, b-a);
+	}
+	
+	@Test
+	public void multi() {
+		System.out.println("Multiplication");
+		Assert.assertEquals(200, b*a);
+	}
+	
+	@Test
+	public void div() {
+		System.out.println("Substraction");
+		Assert.assertEquals(2, b/a);
+	}
+
+}
